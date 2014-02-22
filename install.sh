48976d75b866cfbd5dea1e8e4b0ea8a8c6e611e4
Alex Jose <alexnj@gmail.com>
2014-02-22 09:27:15 -0800

    added .tmux.conf symlink.

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@ echo Symlinking ~/.bash_profile.
 ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
 echo Symlinking ~/.screenrc.
 ln -s ~/.dotfiles/.screenrc ~/.screenrc
+echo Symlinking ~/.tmux.conf.
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 echo Done.
