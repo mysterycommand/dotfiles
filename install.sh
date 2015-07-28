822e7190050fe17c29f950ebf76d557b058c32de
Matt <mhayes@tumblr.com>
2015-07-28 10:58:12 -0700

    Total cleanup.

--- install.sh
+++ install.sh
@@ -1,13 +1,1 @@
 #!/usr/bin/env bash
-echo Symlinking ~/.bash_profile.
-ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
-echo Symlinking ~/.screenrc.
-ln -s ~/.dotfiles/.screenrc ~/.screenrc
-echo Symlinking ~/.tmux.conf.
-ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
-echo Done.
-echo Symlinking ~/.jshintrc.
-ln -s ~/.dotfiles/.jshintrc ~/.jshintrc
-echo Symlinking ~/.editorconfig.
-ln -s ~/.dotfiles/.editorconfig ~/.editorconfig
-echo Done.
