64c475b7937153230c36a9f74d7e30c9beb11250
Matt Hayes <matt@mysterycommand.com>
2019-11-03 09:01:43 -0500

    feat: add a zshrc in here, thanks macOS Catalina

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ ln -s `pwd`/src/bash_profile ~/.bash_profile
 ln -s `pwd`/src/editorconfig ~/.editorconfig
 ln -s `pwd`/src/gitconfig ~/.gitconfig
 ln -s `pwd`/src/gitexcludes ~/.gitexcludes
+ln -s `pwd`/src/zshrc ~/.zshrc
