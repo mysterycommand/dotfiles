f82c0f257238afa2ebde249f1b82895981dea72e
Matt Hayes <matt@mysterycommand.com>
2021-04-22 11:06:12 -0500

    chore: I'm a zshell man now

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-ln -s `pwd`/src/bash_profile ~/.bash_profile
 ln -s `pwd`/src/editorconfig ~/.editorconfig
 ln -s `pwd`/src/gitconfig ~/.gitconfig
 ln -s `pwd`/src/gitexcludes ~/.gitexcludes
